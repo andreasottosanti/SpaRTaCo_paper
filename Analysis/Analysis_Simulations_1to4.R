070c1c60efdb0109ad55e99d4d4b1361fd1d092e
andreasottosanti <sottosanti@stat.unipd.it>
2021-10-22 11:30:31 +0200

    analysis

---
+++ Analysis/Analysis_Simulations_1to4.R
@@ -0,0 +1,126 @@
+#--------------------------------------------------
+# Analyse Simulation Results (Simulations 1,...,4)
+#--------------------------------------------------
+
+library(sparseBC)
+library(blockcluster)
+# The file contains the MVNb model with the slight modification we implementend.
+# See Section 4.2 for more details
+source(".../matrixBC_modified.R")
+
+CER <- function(P, Q){
+  if(length(P) != length(Q)) stop("Vectors have different length")
+  n <- length(P)
+  value <- 0
+  for(i in 1:(n-1)){
+    for(j in (i+1):n){
+      value <- value +
+        abs(as.numeric(P[i] == P[j]) - as.numeric(Q[i] == Q[j]))
+    }
+  }
+  value/(n*(n-1)/2)
+}
+
+Scenario <- 1                  # simulation number
+data.directory <- "..."        # substitute '...' with the path where the data are stored
+results.directory <- "..."     # substitute '...' with the path where the results are stored
+n_replicas <- 10               # the number of replicas of the experiment
+
+loglikelihoods <- rep(-Inf, n_replicas)
+
+# ---Create the matrices where the clustering labels are stored
+Best.Cs <- matrix(0,600,n_replicas)
+Best.Cs.sparseBC <- array(0, c(600, n_replicas, 3))
+Best.Cs.BC <- matrix(0,600,n_replicas)
+Best.Cs.kmeans <- matrix(0,600,n_replicas)
+Best.Cs.LBM <- matrix(0,600,n_replicas)
+Best.Cs.MVNb <- array(0, c(600, n_replicas, 3))
+Best.Ds <- matrix(0,600,n_replicas)
+Best.Ds.sparseBC <- array(0, c(600, n_replicas, 3))
+Best.Ds.BC <- matrix(0,600,n_replicas)
+Best.Ds.kmeans <- matrix(0,600,n_replicas)
+Best.Ds.LBM <- matrix(0,600,n_replicas)
+Best.Ds.MVNb <- array(0, c(600, n_replicas, 3))
+
+# ---Create the matrices where the CER values are stored
+CER.values <- matrix(0,n_replicas,2)
+CER.sparseBC <- array(0, c(n_replicas, 2, 3))
+CER.BC <- matrix(0,n_replicas,2)
+CER.kmeans <- matrix(0,n_replicas,2)
+CER.LBM <- matrix(0,n_replicas,2)
+CER.MVNb <- array(0, c(n_replicas, 2, 3))
+
+for(i in 1:n_replicas){
+  # ---load the simulation replica
+  load(paste(data.directory,"/Scenario",Scenario,"_",i,".Rdata",sep=""))
+
+  # ---load the parallel runs of SpaRTaCo on the same replica
+  file.list <- dir(results.directory)[grep(paste("Scenario",Scenario,"_replica",i,"_",sep=""), dir(results.directory))]
+  for(j in 1:length(file.list)){
+    load(paste(results.directory,"/",file.list[j],sep=""))
+    if(max(results$logL) > loglikelihoods[i]){
+      loglikelihoods[i] <- max(results$logL)
+      Best.Cs[,i] <- results$Cs
+      Best.Ds[,i] <- results$Ds
+      }
+  }
+  CER.values[i,1] <- CER(P = Simulation$original.Cs, Q = Best.Cs[,i])
+  CER.values[i,2] <- CER(P = Simulation$original.Ds, Q = Best.Ds[,i])
+
+  # ---sparseBC
+  sparseBC1 <- sparseBC(Simulation$x, k = 3, r = 3, lambda = 1)
+  sparseBC2 <- sparseBC(Simulation$x, k = 3, r = 3, lambda = 10)
+  sparseBC3 <- sparseBC(Simulation$x, k = 3, r = 3, lambda = 20)
+  Best.Cs.sparseBC[,i,1] <- sparseBC1$Cs
+  Best.Ds.sparseBC[,i,1] <- sparseBC1$Ds
+  Best.Cs.sparseBC[,i,2] <- sparseBC2$Cs
+  Best.Ds.sparseBC[,i,2] <- sparseBC2$Ds
+  Best.Cs.sparseBC[,i,3] <- sparseBC3$Cs
+  Best.Ds.sparseBC[,i,3] <- sparseBC3$Ds
+  CER.sparseBC[i,1,1] <- CER(P = Simulation$original.Cs, Q = sparseBC1$Cs)
+  CER.sparseBC[i,2,1] <- CER(P = Simulation$original.Ds, Q = sparseBC1$Ds)
+  CER.sparseBC[i,1,2] <- CER(P = Simulation$original.Cs, Q = sparseBC2$Cs)
+  CER.sparseBC[i,2,2] <- CER(P = Simulation$original.Ds, Q = sparseBC2$Ds)
+  CER.sparseBC[i,1,3] <- CER(P = Simulation$original.Cs, Q = sparseBC3$Cs)
+  CER.sparseBC[i,2,3] <- CER(P = Simulation$original.Ds, Q = sparseBC3$Ds)
+
+  # ---BC
+  BC <- sparseBC(Simulation$x, k = 3, r = 3, lambda = 0)
+  CER.BC[i,1] <- CER(P = Simulation$original.Cs, Q = BC$Cs)
+  CER.BC[i,2] <- CER(P = Simulation$original.Ds, Q = BC$Ds)
+  Best.Cs.BC[,i] <- BC$Cs
+  Best.Ds.BC[,i] <- BC$Ds
+
+  # ---kmeans
+  kmrow <- kmeans(Simulation$x, centers = 3, nstart = 10)
+  kmcol <- kmeans(t(Simulation$x), centers = 3, nstart = 10)
+  CER.kmeans[i,1] <- CER(P = Simulation$original.Cs, Q = kmrow$clust)
+  CER.kmeans[i,2] <- CER(P = Simulation$original.Ds, Q = kmcol$clust)
+  Best.Cs.kmeans[,i] <- kmrow$clust
+  Best.Ds.kmeans[,i] <- kmcol$clust
+
+  # ---LBM
+  lbm <- coclusterContinuous(data = Simulation$x, nbcocluster = c(3,3))
+  CER.LBM[i,1] <- CER(P = Simulation$original.Cs, Q = lbm@rowclass)
+  CER.LBM[i,2] <- CER(P = Simulation$original.Ds, Q = lbm@colclass)
+  Best.Cs.LBM[,i] <- lbm@rowclass
+  Best.Ds.LBM[,i] <- lbm@colclass
+
+  # ---MVNb
+  MVNb1 <- matrixBC2(x = Simulation$x, k = 3, r = 3, lambda = 1, alpha = .25, beta = .25)
+  MVNb2 <- matrixBC2(x = Simulation$x, k = 3, r = 3, lambda = 10, alpha = 2.5, beta = 2.5)
+  MVNb3 <- matrixBC2(x = Simulation$x, k = 3, r = 3, lambda = 20, alpha = 5, beta = 5)
+  CER.MVNb[i,1,1] <- CER(P = Simulation$original.Cs, Q = MVNb1$Cs)
+  CER.MVNb[i,2,1] <- CER(P = Simulation$original.Ds, Q = MVNb1$Ds)
+  CER.MVNb[i,1,2] <- CER(P = Simulation$original.Cs, Q = MVNb2$Cs)
+  CER.MVNb[i,2,2] <- CER(P = Simulation$original.Ds, Q = MVNb2$Ds)
+  CER.MVNb[i,1,3] <- CER(P = Simulation$original.Cs, Q = MVNb3$Cs)
+  CER.MVNb[i,2,3] <- CER(P = Simulation$original.Ds, Q = MVNb3$Ds)
+  Best.Cs.MVNb[,i,1] <- MVNb1$Cs
+  Best.Ds.MVNb[,i,1] <- MVNb1$Ds
+  Best.Cs.MVNb[,i,2] <- MVNb2$Cs
+  Best.Ds.MVNb[,i,2] <- MVNb2$Ds
+  Best.Cs.MVNb[,i,3] <- MVNb3$Cs
+  Best.Ds.MVNb[,i,3] <- MVNb3$Ds
+}
+
